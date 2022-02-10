36fbc91da9883ffa3709c0db5286872ead5ab5da
Ethel3 <akrasiethel4@gmail.com>
2022-02-10 16:40:24 +0000

    update

--- js/app.js
+++ js/app.js
@@ -16,3 +16,24 @@ var today =new Date();
 var year =today.getFullYear();
 var yearSpan = document.getElementById("year");
 yearSpan.innerText = year;
+
+// js for product gallery//
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("small-img");
+
+SmallImg[0].onclick = function()
+{
+    ProductImg.src = SmallImg[0].getAttribute("src");
+}
+SmallImg[1].onclick = function()
+{
+    ProductImg.src = SmallImg[1].getAttribute("src");
+}
+SmallImg[2].onclick = function()
+{
+    ProductImg.src = SmallImg[2].getAttribute("src");
+}
+SmallImg[3].onclick = function()
+{
+    ProductImg.src = SmallImg[3].getAttribute("src");
+}
