b6d267d243b5a88f2af90e190fb8768da97708fb
Ethel3 <akrasiethel4@gmail.com>
2022-02-13 22:22:25 +0000

    updated

--- js/app.js
+++ js/app.js
@@ -37,3 +37,23 @@ SmallImg[3].onclick = function()
 {
     ProductImg.src = SmallImg[3].getAttribute("src");
 }
+//js for toggle Form//
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+        function register(){
+            RegForm.style.transform = "translateX(0px)";
+            LoginForm.style.transform = "translateX(0px)";
+            Indicator.style.transform = "translateX(100px)";
+        }
+
+        function login(){
+            RegForm.style.transform = "translateX(300px)";
+            LoginForm.style.transform = "translateX(300px)";
+            Indicator.style.transform = "translateX(0px)";
+        }
+
+
+
+
